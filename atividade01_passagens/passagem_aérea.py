243856dd15e9588c453d54070669ac938fc18fff
beatrizanalista <beatriz.vivi0310@gmail.com>
2024-11-29 21:28:36 -0300

    iniciando_atividade_passagens_aérias

---
+++ atividade01_passagens/passagem_aérea.py
@@ -0,0 +1,11 @@
+# Atividade 001 - Passagens Aréas
+# Curso: Desenvolvimento de sistemas
+# Data:29/11
+# Autor: Beatriz victoria
+
+# - normalição
+# A atividade proposta consiste em desenvolver um sistema de gerenciamento de passagens aéreas utilizando Python e o banco de dados SQLite3,
+# aplicando o conceito de CRUD (Create, Read, Update e Delete) para manipulação dos dados. O sistema deve permitir o cadastro de informações essenciais,
+# como nome do passageiro, número do voo, destino, data e hora da viagem, preço do bilhete, entre outros detalhes relevantes. Com a estrutura de CRUD,
+# os usuários devem poder adicionar novos registros de passagens, visualizar a lista de passagens cadastradas, atualizar informações (por exemplo, ajustar horários ou destinos) e, 
+# quando necessário, excluir registros de passagens.
