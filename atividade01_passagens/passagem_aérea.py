7abf668159c8bb17e3b047ed830dadaf60244fcb
beatrizanalista <beatriz.vivi0310@gmail.com>
2024-12-03 21:08:11 -0300

    cont.ativ_sqlite3

--- atividade01_passagens/passagem_aérea.py
+++ atividade01_passagens/passagem_aérea.py
@@ -12,17 +12,24 @@
 
 import os
 import sqlite3 
-
+from prettytable import PrettyTable
 os.system('cls')
 
 conn = sqlite3.connect("c:/repositorios/banco_de_dados_relacional/meu_banco.db")
 
 cursor = conn.cursor()
 
+tabela = PrettyTable()
+
+colunas = [descricao[0] for descricao in cursor.description]
+
+for row in 'resultados':
+    tabela.add_row(row)
+
 def criando_bd():
     sqlite3.cursor('''CREATE TABLE passagem (
     'id_bilhete'int,
-    'nome_viajante'int,
+    'nome_viajante' text not null,
     'id_cadeira'int,
     'id_preco'decimal,
     'direto_ou_escala'into,
@@ -30,42 +37,57 @@ def criando_bd():
     'ida'int,
     'data'int,
     'hora'float,
-    'Assento',
+    'Assento,text',
     'portao_emparque'int,
     'destino'int,
     'classe de Assento',
-    ''');
-def criando_bd():
-    sqlite3('''CREATE TABLE escala (
+    );
+''')
+                                        
+cursor.execute('''
+    'CREATE TABLE  IF NOT EXISTS escala (
     'id,int'
     'data,float',
     'hora,decimal',
     'portao_embarque,int',
     'companhia aerea'int',
     'local_embarque,int',
-    ''');
+    );
+''')
 
-def criando_bd():
-    sqlite3('''CREATE TABLE viajante (
-    'id,int',
-     ''' );
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS viajante (
+    'id PRIMARY KEY AUTOINCREMENT, 
+    'numero_embarque',
+    'companhia_aerea',
+    'local_embarque,int',
+    'data,float',   
+    'hora,decimal'   
+    );
+''')
     
-def criando_bd():
-    sqlite3('''CREATE TABLE empresa (
-    'gol Ailines',
-    'Latam Airlines',
-    'Azul linhas Aereas',
-    'American Airlines',
-    'Air france',
-    'Qatar airways'
-    ''');
-    
-def criando_bd():
-    sqlite3(''' CREATE TABLE classe(
-        ''');
+empresa = [
+    ("id"),
+    ("gol Ailines"),
+    ("Latam Airlines"),                              
+    ("Azul linhas Aereas"),
+    ("American Airlines"),
+    ("Air france"),
+    ("Qatar airways")
+]
+                   
+cursor.execute('''
+     CREATE TABLE classe (
+    ("id_classe"),
+    ("classe economica"),  
+    ("classe executiva"),
+    ("primeira classe")
+    );
+''')
 
-def criando_bd():
-    sqlite3(''' CREATE TABLE preco (
-        ''');
-    
-   
+preco = [
+   ("id_preco"),
+   ("1.000"),
+   ("3.500"),
+   ("6.000")
+]
