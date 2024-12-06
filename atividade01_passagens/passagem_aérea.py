b68c0aa18b58c3116a5fdf6ac19abf1b2fbc1fa1
beatrizanalista <beatriz.vivi0310@gmail.com>
2024-12-05 21:34:06 -0300

    atividade01_cont

--- atividade01_passagens/passagem_aérea.py
+++ atividade01_passagens/passagem_aérea.py
@@ -12,82 +12,60 @@
 
 import os
 import sqlite3 
-from prettytable import PrettyTable
 os.system('cls')
 
-conn = sqlite3.connect("c:/repositorios/banco_de_dados_relacional/meu_banco.db")
+conn = sqlite3.connect("C:/repositorio_beatriz/banco_de_dados_relacional/atividade01_passagens/viajem.db")
 
 cursor = conn.cursor()
 
-tabela = PrettyTable()
-
-colunas = [descricao[0] for descricao in cursor.description]
-
-for row in 'resultados':
-    tabela.add_row(row)
-
 def criando_bd():
-    sqlite3.cursor('''CREATE TABLE passagem (
-    'id_bilhete'int,
-    'nome_viajante' text not null,
-    'id_cadeira'int,
-    'id_preco'decimal,
-    'direto_ou_escala'into,
-    'ida_volta'int,
-    'ida'int,
-    'data'int,
-    'hora'float,
-    'Assento,text',
-    'portao_emparque'int,
-    'destino'int,
-    'classe de Assento',
-    );
-''')
-                                        
-cursor.execute('''
-    'CREATE TABLE  IF NOT EXISTS escala (
-    'id,int'
-    'data,float',
-    'hora,decimal',
-    'portao_embarque,int',
-    'companhia aerea'int',
-    'local_embarque,int',
+    cursor.execute('''CREATE TABLE passagem (
+    id integer PRIMARY KEY, 
+    id_viajante integer,
+    id_cadeira integer,
+    id_companhia integer,
+    id_preco text,
+    data_ida text,
+    data_volta text, 
+    assento text,
+    portao_embarque text,
+    classe text
     );
 ''')
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS viajante (
-    'id PRIMARY KEY AUTOINCREMENT, 
-    'numero_embarque',
-    'companhia_aerea',
-    'local_embarque,int',
-    'data,float',   
-    'hora,decimal'   
+    id integer PRIMARY KEY, 
+    nome_viajante text,
+    numero_embarque text,
+    origem text,
+    destino text,
+    idade integer
     );
 ''')
     
-empresa = [
-    ("id"),
-    ("gol Ailines"),
-    ("Latam Airlines"),                              
-    ("Azul linhas Aereas"),
-    ("American Airlines"),
-    ("Air france"),
-    ("Qatar airways")
-]
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS companhia_aerea (
+    id integer PRIMARY KEY,
+    nome_da_empresa TEXT
+    );
+''')              
                    
 cursor.execute('''
-     CREATE TABLE classe (
-    ("id_classe"),
-    ("classe economica"),  
-    ("classe executiva"),
-    ("primeira classe")
+    CREATE TABLE IF NOT EXISTS classe (
+    id_classe integer PRIMARY KEY,
+    classe text
     );
 ''')
 
-preco = [
-   ("id_preco"),
-   ("1.000"),
-   ("3.500"),
-   ("6.000")
-]
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS preco (
+    id integer PRIMARY KEY,
+    preco_da_passagem text,
+    tipo text           
+    );
+''')
+
+conn.commit()
+
+conn.close()
