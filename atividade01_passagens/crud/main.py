4002a1b985b23c68d039bfa42a1abcde1e8cbf23
Beatriz-Victoria2 <166769545+Beatriz-Victoria2@users.noreply.github.com>
2025-01-21 17:43:27 -0300

    correçao...

--- atividade01_passagens/crud/main.py
+++ atividade01_passagens/crud/main.py
@@ -6,11 +6,10 @@ import os
 
 
 while True:
-    os.system("cls")
     print("\n=== Sistema de Passagens Aéreas ===")
     print("1. Cadastrar imformações para a compra")
     print("2. Consultar passagem")
-    print("3. Atualizar as Informações")
+    print("3. Atualizar as Imformações")
     print("4. Excluir informação do resgistro da passagem")
     print("5. Sair do site")
     opcao = input("Aperte uma opção: ")
@@ -21,7 +20,7 @@ while True:
     elif opcao == '2':
         pergunta = input("Escolha uma tabela para vizualizar: ")
         visualizar(pergunta)
-        input('Presione ENTER para voltar: ')
+        input('Pressione ENTER para voltar: ')
     elif opcao == '3':
         pergunta = input("Escolha uma tabela você deseja atualizar: ")
         atualizar(pergunta)
